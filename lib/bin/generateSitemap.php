3f8fcac4c1f450e899cd0efb7a858314f322dc43
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-10-21 15:25:46 +0200

    [REFACTOR] Dispatch des fonctions du service website_WebsiteModuleService

--- lib/bin/generateSitemap.php
+++ lib/bin/generateSitemap.php
@@ -5,5 +5,5 @@ $sitemap = seo_persistentdocument_sitemap::getInstanceById($sitemapId);
 $lang = $sitemap->getWebsiteLang();
 RequestContext::getInstance()->setLang($lang);
 $website = $sitemap->getWebsite();
-website_WebsiteModuleService::getInstance()->setCurrentWebsite($website);
+website_WebsiteService::getInstance()->setCurrentWebsite($website);
 echo seo_SitemapService::getInstance()->appendUrl($sitemap, $tmpFile, $modelName, $offset, $chunkSize);
