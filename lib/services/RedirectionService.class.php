36e35d2f729b9f2648307ef5e4e4e1679404cc2b
Loïc Couturier <loic.couturier@rbs.fr>
2012-10-04 10:22:58 +0200

    [FIX #68188] Redirection vers une ancre -> caractère '#' reste encodé

--- lib/services/RedirectionService.class.php
+++ lib/services/RedirectionService.class.php
@@ -94,11 +94,11 @@ class seo_RedirectionService extends f_persistentdocument_DocumentService
 				{
 					list($oldUrl, $queryString) = explode('?', $oldUrl);
 					$data[] = 'RewriteCond %{QUERY_STRING} ^'.preg_quote($queryString).'$';
-					$data[] = 'RewriteRule ^' . preg_quote($oldUrl) . '$ ' . $newUrl . ' [L,R=301]';
+					$data[] = 'RewriteRule ^' . preg_quote($oldUrl) . '$ ' . $newUrl . ' [L,NE,R=301]';
 				}
 				else 
 				{
-					$data[] = 'RewriteRule ^' . preg_quote($oldUrl) . '$ ' . $newUrl . ' [L,R=301]';
+					$data[] = 'RewriteRule ^' . preg_quote($oldUrl) . '$ ' . $newUrl . ' [L,NE,R=301]';
 				}
 			}
 		}
